c05c98bc676fa2e311edc3f5fbd2a1d999bb7925
Alex Langberg <mail@alexlangberg.dk>
2015-08-30 23:35:36 +0200

    Writing works.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,46 @@
 'use strict';
 
+/*eslint-disable vars-on-top */
+
 var test = require('tape');
 var subject = require('../');
+var Vinyl = require('vinyl');
+var through2 = require('through2');
+var tap = require('gulp-tap');
+var fs = require('fs');
 
-test("returns true", function(t) {
+var log = function(file) {
+  for (var property in file) {
+    if (file.hasOwnProperty(property)) {
+      console.log(property, file[property]);
+    }
+  }
+};
+
+test('returns true', function(t) {
   t.plan(1);
+  t.equal(typeof subject.write, 'function');
+});
 
-  t.equal(subject(), true);
+test('can write file', function(t) {
+  t.plan(2);
+
+  var file = new Vinyl({
+    'path': 'tests.json',
+    'contents': new Buffer('{"foo":"barssss"}')
+  });
+
+  var input = through2.obj();
+  input.push(file);
+  input.push(null);
+  input
+    .pipe(subject.write({ 'root': './testdata' }))
+    .pipe(tap(function(obj) {
+      //log(obj);
+      var filename = obj.history[obj.history.length - 1];
+      var file = fs.readFileSync(filename);
+
+      t.true(filename.indexOf('.json.gz') > -1);
+      t.true(Buffer.isBuffer(file));
+    }));
 });
